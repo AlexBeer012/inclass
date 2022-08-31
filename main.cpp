53bdb746e29cd718401cc2e67b1864e95f2b885c
AlexBeer012 <alexbeer37@gmail.com>
2022-08-31 10:12:51 -0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+//Alex Beer
+// get git (bash) and reach ch 1 of the textbook. Hello world program.
+//emacs or vim for terminal. Figure out terminal
+//Aug 31st, 2022
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+
+//man or --help in terminal means manual
+//hw is tell git and github to work
